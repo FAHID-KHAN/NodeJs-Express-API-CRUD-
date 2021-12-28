6b130a72ea80c6587b4f860d28c207549551b521
Fahid Khan <fahid.khan@tuni.fi>
2021-12-28 21:42:02 +0200

    routes updated,app.js updated with sequelize and new dependencies added

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8080"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./models");
+db.sequelize.sync;
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Todo Backend Rest API." });
+});
+
+require("./routes/routes.js")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
