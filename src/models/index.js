2789336df4a2703bd994c9c966c62f6399987d6e
Fahid Khan <fahid.khan@tuni.fi>
2021-12-28 20:54:47 +0200

    Todo db model added

---
+++ src/models/index.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("postgres://postgres:postgres@localhost:5000/backend");
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = require("./todo.js")(sequelize, Sequelize);
+
+module.exports = db;
